eb4708911791b36c4cc41b5a5adb39db2aee7cdb
adarshhtiwari <143872020+adarshhtiwari@users.noreply.github.com>
2023-11-14 07:21:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,142 @@
+// return the value of slider to span
+const slider = document.getElementById("slider");
+  const slidervalue = document.getElementById("slider-value");
+  
+  slider.addEventListener("input", function() {
+    slidervalue.textContent = slider.value;
+
+  });
+
+  
+
+
+
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+  
+  const boxes = document.querySelectorAll('.box');
+  
+  function updateBoxColoring() {
+    let selectedCount = 0;
+    checkboxes.forEach((checkbox) => {
+      if (checkbox.checked) {
+        selectedCount++;
+      }
+    });
+  
+    const sliderValue = parseInt(slider.value);
+  
+    boxes.forEach((box, index) => {
+      if (selectedCount === 0 || sliderValue < 4) {
+        box.classList.remove('colored');
+      } else if (index < selectedCount) {
+        box.classList.add('colored');
+      } else {
+        box.classList.remove('colored');
+      }
+    });
+  }
+  
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', updateBoxColoring);
+  });
+  
+  slider.addEventListener('input', updateBoxColoring);
+
+
+
+//To generate random password
+function generatePassword() {
+    var includeUppercase = document.getElementById("includeUppercase").checked;
+    var includeLowercase = document.getElementById("includeLowercase").checked;
+    var includeNumbers = document.getElementById("includeNumbers").checked;
+    var includeSymbols = document.getElementById("includeSymbols").checked;
+    var passwordLength = document.getElementById("slider").value;
+
+
+    let selectedCount = 0;
+
+      checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+          selectedCount++;
+        }
+      });
+   
+    
+
+
+    var characterSet = "";
+    var characterSet1 = "";
+    var characterSet2 = "";
+    var characterSet3 = "";
+    var characterSet4 = "";
+    var password = "";
+
+
+    if (includeUppercase) {
+      characterSet1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      var randomIndex = Math.floor(Math.random() * characterSet1.length);
+      password += characterSet1.charAt(randomIndex);
+    }
+    if (includeLowercase) {
+      characterSet2 = "abcdefghijklmnopqrstuvwxyz";
+      var randomIndex = Math.floor(Math.random() * characterSet2.length);
+      password += characterSet2.charAt(randomIndex);
+    }
+    if (includeNumbers) {
+      characterSet3 = "0123456789";
+      var randomIndex = Math.floor(Math.random() * characterSet3.length);
+      password += characterSet3.charAt(randomIndex);
+    }
+    if (includeSymbols) {
+      characterSet4 = "!@#$%^&*()";
+      var randomIndex = Math.floor(Math.random() * characterSet4.length);
+      password += characterSet4.charAt(randomIndex);
+    }
+    characterSet = characterSet1 + characterSet2 + characterSet3 + characterSet4;
+
+    if(passwordLength<4){
+      if(passwordLength<selectedCount){
+          return;
+      }else{
+        for (var i = 0; i < passwordLength-selectedCount; i++) {
+          var randomIndex = Math.floor(Math.random() * characterSet.length);
+          password += characterSet.charAt(randomIndex);
+        
+      }
+      }
+    }
+    else if(passwordLength>=4){
+      for (var i = 0; i < passwordLength-selectedCount; i++) {
+        var randomIndex = Math.floor(Math.random() * characterSet.length);
+        password += characterSet.charAt(randomIndex);
+      
+      
+    }}
+
+    document.getElementById("output").textContent = password;
+  }
+
+  var generateBtn = document.getElementById("generate");
+  generateBtn.addEventListener("click", generatePassword);
+
+
+
+
+ 
+
+//copy to clipboard function
+  document.getElementById("copy").addEventListener("click", copyToClipboard);
+  function copyToClipboard() {
+    var passwordDisplay = document.getElementById("output");
+    var password = passwordDisplay.textContent;
+  
+    var textarea = document.createElement("textarea");
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+  
+    alert("Password copied to clipboard: " + password);
+  }
